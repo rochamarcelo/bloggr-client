280140aaf59eef1c59a510297716cb6c12180e11
Marcelo Rocha <mrcodex@msn.com>
2014-08-09 17:16:09 -0300

    New Post route and delete action

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,32 @@
 App.PostController = Ember.ObjectController.extend({
   isEditing: false,
-
   actions: {
     edit: function() {
       this.set('isEditing', true);
     },
-
     doneEditing: function() {
       var model = this.get('model');
       this.set('isEditing', false);
-      console.log(model.get('author'));
-
       model.save();
-      console.log(model.get('author'));
+    }
+  }
+});
+
+App.PostsController = Ember.ObjectController.extend({
+  actions: {
+    delete: function(model) {
+      model.deleteRecord();
+      model.save();
+    }
+  }
+});
+
+App.NewPostController = Ember.ObjectController.extend({
+  actions: {
+    done: function(router) {
+        var model = this.get('model');
+        model.save();
+        this.transitionToRoute('/posts/view/' + model.get('id'));
     }
   }
 });
--- js/routes.js
+++ js/routes.js
@@ -1,36 +1,37 @@
-App.IndexRoute = Ember.Route.extend({
-  beforeModel: function() {
-    // this.transitionTo('posts');
-    var models = this.store.find('post');
-    console.log(models);
-
-    models = this.store.find('author');
-    console.log(models);
-  }
-});
-
 App.Router.map(function() {
   this.resource('about');
   this.resource('posts', function() {
-    this.resource('post', { path: ':post_id' });
+    this.resource('post', { path: '/view/:post_id' });
+    this.resource('new_post', { path: '/new' });
   });
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
-    varr = this.store.find('author');
-    console.log(varr);
     return this.store.find('post');
-  },
-  afterModel: function (posts, transition) {
-    if ( posts.get('length') >= 1 ) {
-      this.transitionTo('post', posts.get('firstObject'));
-    }
   }
 });
 
 App.PostRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('post', params.post_id);
+  }
+});
+
+App.NewPostRoute = Ember.Route.extend({
+  model: function(params) {
+     var post = this.store.createRecord(
+        'post',
+        {
+            title: 'New post',
+            date: new Date(),
+            excerpt: '',
+            body: ''
+        }
+    );
+    this.store.find('author', 2).then(function(author){
+        post.set('author', author);
+    });
+    return post;
   }
 });
